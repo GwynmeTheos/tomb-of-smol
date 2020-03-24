f912d4242c09eb6786a926e892d2c3b0fb41c92c
GwynmeTheos <ldrj.leoleo@gmail.com>
2020-03-24 16:54:23 -0300

    feature/Load game, JSON-style data storage system

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-import os, colorama, keyboard, engine, player
+import os, colorama, engine, player, json
 
 def Start():
     os.system("CLS")
@@ -10,7 +10,6 @@ def Start():
     print()
     print("(" + colorama.Style.BRIGHT +"N" + colorama.Style.RESET_ALL + ")ew Game")
     print("(" + colorama.Style.BRIGHT +"L" + colorama.Style.RESET_ALL + ")oad Game")
-    print("(" + colorama.Style.BRIGHT +"O" + colorama.Style.RESET_ALL + ")ptions")
     print("(" + colorama.Style.BRIGHT +"Q" + colorama.Style.RESET_ALL + ")uit")
     print()
     print("Input a highlighted key.")
@@ -23,12 +22,8 @@ def Start():
             engine.StateController(user)
                 
         elif selection == 'l':
-            # user = LoadGame()
-            # engine.StateController(user)
-            continue
-
-        elif selection == 'o':
-            Options()
+            user = LoadGame()
+            engine.StateController(user)
             continue
                 
         elif selection == 'q':
@@ -36,8 +31,6 @@ def Start():
         
         else: 
             continue
-
-
 
 def NewGameStart():
     os.system("CLS")
@@ -191,11 +184,69 @@ def NewGameStart():
     newPlayer = player.Player(name, playerClass, difficulty, 1, strength, dexterity, intelligence, baseHP, baseHP, baseMP, baseMP, skills, weapon, armor, accessory)
     return newPlayer
 
-
-
 def LoadGame():
-    pass
-
-def Options():
     os.system("CLS")
+
+    savefile0 = open("python\\saves\\save0", "r")
+    savedata0 = json.load(savefile0)
+    savefile1 = open("python\\saves\\save1", "r")
+    savedata1 = json.load(savefile1)
+    savefile2 = open("python\\saves\\save2", "r")
+    savedata2 = json.load(savefile2)
+    savefile3 = open("python\\saves\\save3", "r")
+    savedata3 = json.load(savefile3)
+    savefile4 = open("python\\saves\\save4", "r")    
+    savedata4 = json.load(savefile4)
+    savefile5 = open("python\\saves\\save5", "r")
+    savedata5 = json.load(savefile5)
+
+    print("Select a save: ")
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "0" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata0))
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "1" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata1))
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "2" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata2))
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "3" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata3))
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "4" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata4))
+    print()
+    print("File ("+ colorama.Style.BRIGHT + "5" + colorama.Style.RESET_ALL +")")
+    print(DataLookup(savedata5))
+    print()
+
+    selection = str(input())
+    while True:
+        if selection == "0":
+            user = player.Player(savedata0['name'], savedata0['playerClass'], savedata0['difficulty'], savedata0['level'], savedata0['maxHP'], savedata0['currentHP'], savedata0['maxMP'], savedata0['currentMP'], savedata0['strength'], savedata0['dexterity'], savedata0['intelligence'], set(savedata0['skills']), savedata0['weapon'], savedata0['armor'], savedata0['accessory'])
+            return user
+        elif selection == "1":
+            user = player.Player(savedata1['name'], savedata1['playerClass'], savedata1['difficulty'], savedata1['level'], savedata1['maxHP'], savedata1['currentHP'], savedata1['maxMP'], savedata1['currentMP'], savedata1['strength'], savedata1['dexterity'], savedata1['intelligence'], set(savedata1['skills']), savedata1['weapon'], savedata1['armor'], savedata1['accessory'])
+            return user        
+        elif selection == "2":
+            user = player.Player(savedata2['name'], savedata2['playerClass'], savedata2['difficulty'], savedata2['level'], savedata2['maxHP'], savedata2['currentHP'], savedata2['maxMP'], savedata2['currentMP'], savedata2['strength'], savedata2['dexterity'], savedata2['intelligence'], set(savedata2['skills']), savedata2['weapon'], savedata2['armor'], savedata2['accessory'])
+            return user
+        elif selection == "3":
+            user = player.Player(savedata3['name'], savedata3['playerClass'], savedata3['difficulty'], savedata3['level'], savedata3['maxHP'], savedata3['currentHP'], savedata3['maxMP'], savedata3['currentMP'], savedata3['strength'], savedata3['dexterity'], savedata3['intelligence'], set(savedata3['skills']), savedata3['weapon'], savedata3['armor'], savedata3['accessory'])
+            return user
+        elif selection == "4":
+            user = player.Player(savedata4['name'], savedata4['playerClass'], savedata4['difficulty'], savedata4['level'], savedata4['maxHP'], savedata4['currentHP'], savedata4['maxMP'], savedata4['currentMP'], savedata4['strength'], savedata4['dexterity'], savedata4['intelligence'], set(savedata4['skills']), savedata4['weapon'], savedata4['armor'], savedata4['accessory'])
+            return user
+        elif selection == "5":
+            user = player.Player(savedata5['name'], savedata5['playerClass'], savedata5['difficulty'], savedata5['level'], savedata5['maxHP'], savedata5['currentHP'], savedata5['maxMP'], savedata5['currentMP'], savedata5['strength'], savedata5['dexterity'], savedata5['intelligence'], set(savedata5['skills']), savedata5['weapon'], savedata5['armor'], savedata5['accessory'])
+            return user
+        else:
+            continue
+
+def DataLookup(jsonDict):
+    if jsonDict["written"] == False:
+        return "No save data."
+    else:
+        return str(jsonDict["name"]) + " - Level " + str(jsonDict["level"])
 
